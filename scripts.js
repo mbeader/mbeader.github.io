98657f4d4fbc207a61785eb9d4e96c57b9a718b8
mbeader <matt.beader@gmail.com>
2019-07-24 16:03:13 -0400

    add photos page

--- scripts.js
+++ scripts.js
@@ -1,5 +1,32 @@
 var bg = ['chase', 'dog', 'lmp3', 'mazda', 'mods', 'rebellion', 'thompson', 'wec', 'wet'];
+var info;
 
 document.addEventListener('DOMContentLoaded', function() {
-  document.body.style = "background: url('bg/" + bg[Math.floor(Math.random()*bg.length)] + ".jpg') no-repeat center center fixed; background-size: cover;";
+  let index = Math.floor(Math.random()*bg.length);
+  document.body.style = "background: url('bg/" + bg[index] + ".jpg') no-repeat center center fixed; background-size: cover;";
+  if(window.location.pathname == 'photos.html') {
+    loadInfo(index);
+    document.getElementById('next').addEventListener('click', function(){
+      displayInfo(Math.floor(Math.random()*bg.length));
+    });
+  }
 });
+
+function loadInfo(i) {
+  let req = new XMLHttpRequest();
+  req.addEventListener("load", function(){
+    info = JSON.parse(req.responseText);
+    displayInfo(i);
+  });
+  req.open('GET', 'https://mbeader.github.io/info.json');
+  req.send();
+}
+
+function displayInfo(i) {
+  document.getElementById('date').textContent = info[i].date;
+  document.getElementById('track').textContent = info[i].track;
+  document.getElementById('location').textContent = info[i].location;
+  document.getElementById('event').textContent = info[i].event;
+  document.getElementById('series').textContent = info[i].series;
+  document.getElementById('subject').textContent = info[i].subject;
+}
